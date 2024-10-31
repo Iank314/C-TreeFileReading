2eb76ee851b00d5b8a550df3f7f782b42a9f2a9a
Ian Kaufman <Kaufmanian49@gmail.com>
2024-10-31 23:41:31 +0000

    pls

--- src/qtree.c
+++ src/qtree.c
@@ -63,47 +63,42 @@ QTNode *create_quadtree(Image *image, double max_rmse)
     return create_quadtree_helper(image, 0, 0, get_image_width(image), get_image_height(image), max_rmse);
 }
 
-
-QTNode *get_child1(QTNode *node) 
+QTNode *get_child1(QTNode *node)
 {
-    (void)node;
-    return NULL;
+    return node ? node->children[0] : NULL;
 }
 
-
-QTNode *get_child2(QTNode *node) 
+QTNode *get_child2(QTNode *node)
 {
-    (void)node;
-    return NULL;
+    return node ? node->children[1] : NULL;
 }
 
-
-QTNode *get_child3(QTNode *node) 
+QTNode *get_child3(QTNode *node)
 {
-    (void)node;
-    return NULL;
+    return node ? node->children[2] : NULL;
 }
 
-
-QTNode *get_child4(QTNode *node) 
+QTNode *get_child4(QTNode *node)
 {
-    (void)node;
-    return NULL;
+    return node ? node->children[3] : NULL;
 }
 
-
-unsigned char get_node_intensity(QTNode *node) 
+unsigned char get_node_intensity(QTNode *node)
 {
-    (void)node;
-    return 0;
+    return node ? node->intensity : 0;
 }
 
-
-void delete_quadtree(QTNode *root) 
+void delete_quadtree(QTNode *root)
 {
-    (void)root;
+    if (root)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            delete_quadtree(root->children[i]);
+        }
+        free(root);
+    }
 }
-
 
 void save_qtree_as_ppm(QTNode *root, char *filename) 
 {
